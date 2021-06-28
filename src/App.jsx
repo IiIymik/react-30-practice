00542306f84575ae25bcbef974c543157bab81e1
IiIymik <sambotemp@gmail.com>
2021-06-28 22:22:12 +0300

    add file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+  import Wrapper from "./components/Wrapper/Wrapper";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Wrapper></Wrapper>
     </div>
   );
 }
---
+++ src/components/Wrapper/Wrapper.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import Sidebar from "../Sidebar/Sidebar";
+import Main from "../Main/Main";
+import { Container } from "./Wrapper.styles";
+const Wrapper = () => {
+  return (
+    <Container>
+      <Sidebar></Sidebar>
+      <Main />
+    </Container>
+  );
+};
+
+export default Wrapper;
